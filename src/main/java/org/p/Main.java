198d3f9a9b0a0ea10d35ba8eb9b5621192a507d6
Proto <protogithub@gmail.com>
2023-09-17 16:13:56 +0100

    Initial commit

---
+++ src/main/java/org/p/Main.java
@@ -0,0 +1,39 @@
+package org.p;
+
+import org.apache.tools.ant.types.Commandline;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> allArgs = new ArrayList<>();
+        allArgs.add("java");
+        allArgs.add("-jar");
+        allArgs.add(System.getProperty("user.home") + "\\DreamBot\\BotData\\client.jar");
+
+
+        try {
+            String source = new File(Main.class.getProtectionDomain().getCodeSource().getLocation()
+                    .toURI().getPath()).getAbsoluteFile().toString().replace("RuneLite.jar", "db_config.txt");
+            BufferedReader Buff = new BufferedReader(new FileReader(source));
+            String data = Buff.readLine();
+            if (!data.isEmpty()) {
+                String[] finalData = Commandline.translateCommandline(data);
+                allArgs.addAll(Arrays.asList(finalData));
+            }
+
+            ProcessBuilder processBuilder = new ProcessBuilder(allArgs);
+            Process process = processBuilder.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
